301b5ff23a429ed4bb4980fe68c898d47c829000
Egor Ananyin <ananinegor@gmail.com>
2024-02-24 20:58:38 +0300

    Trying to make a cube

--- cube-lib/src/main.cpp
+++ cube-lib/src/main.cpp
@@ -6,6 +6,9 @@
 #include <iostream>
 #include <sstream>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 class ShaderProgram {
 public:
     ShaderProgram(char const* vertex, char const* fragment) {
@@ -62,6 +65,12 @@ public:
         glDeleteProgram(m_programId);
     }
 
+    void setUniformMatrix4fv(char const* name, glm::mat4x4& mat) {
+        glUseProgram(m_programId);
+        auto loc = glGetUniformLocation(m_programId, name);
+        glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(mat));
+    }
+
 private:
     int m_programId;
 };
@@ -79,14 +88,18 @@ public:
     Model() {
         glGenBuffers(1, &m_vbo);
         glGenVertexArrays(1, &m_vao);
+        glGenBuffers(1, &m_ebo);
     }
 
-    Model(void* vertexData, int size, int count) {
+    Model(void* vertexData, void* indexData, int vertexSize, int indexSize, int count) {
         glGenBuffers(1, &m_vbo);
         glGenVertexArrays(1, &m_vao);
+        glGenBuffers(1, &m_ebo);
 
         glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-        glBufferData(GL_ARRAY_BUFFER, size, vertexData, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, vertexSize, vertexData, GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexSize, indexData, GL_STATIC_DRAW);
         m_count = count;
     }
 
@@ -111,7 +124,8 @@ public:
         glBindVertexArray(m_vao);
         if (m_program)
             m_program->use();
-        glDrawArrays(GL_TRIANGLES, 0, m_count);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+        glDrawElements(GL_TRIANGLES, m_count, GL_UNSIGNED_INT, nullptr);
     }
 
     ~Model() {
@@ -122,8 +136,11 @@ public:
 private:
     GLuint m_vbo;
     GLuint m_vao;
+    GLuint m_ebo;
     ShaderProgram* m_program;
     int m_count;
+
+    glm::vec3 m_position;
 };
 
 int main()
@@ -131,24 +148,91 @@ int main()
     auto window = sf::Window{ { 1920u, 1080u }, "Cube" };
     window.setFramerateLimit(144);
 
+    glEnable(GL_DEPTH_TEST);
+
+    // float vertices[] = {
+    //     -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+    //     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+    //     0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+    // }; 
+
+    // int indices[] = {
+    //     0, 1, 2,
+    // };
+
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-        0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-    }; 
-    
+        // face 1
+        0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        // face 2
+        0.0f, 0.0f, 0.5f, 1.0f, 0.5f, 0.0f,
+        0.5f, 0.0f, 0.5f, 1.0f, 0.5f, 0.0f,
+        0.0f, 0.5f, 0.5f, 1.0f, 0.5f, 0.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 0.5f, 0.0f,
+        // face 3
+        0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.5f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+        // face 4
+        0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+        0.5f, 0.0f, 0.5f, 0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+        // face 5
+        0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+        0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.5f, 1.0f, 1.0f, 0.0f,
+        0.5f, 0.0f, 0.5f, 1.0f, 1.0f, 0.0f,
+        // face 6
+        0.0f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f,
+        0.0f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f,
+    };
+
+    unsigned int indices[] = {
+        // face 1
+        0, 1, 2,
+        1, 2, 3,
+        // face 2
+        4, 5, 6,
+        5, 6, 7,
+        // face 3
+        8, 9, 10,
+        9, 10, 11,
+        // face 4
+        12, 13, 14,
+        13, 14, 15,
+        // face 5
+        16, 17, 18,
+        17, 18, 19,
+        // face 6
+        20, 21, 22,
+        21, 22, 23,
+    };
+
     // vertex attribs
     VertexAttrib pos = { .size = 3, .type = GL_FLOAT, .stride = 6 * sizeof(float)};
     VertexAttrib col = { .size = 3, .type = GL_FLOAT, .stride = 6 * sizeof(float), .pointer = (void*)(3 * sizeof(float))};
 
     // shader program
     ShaderProgram program("vertex.glsl", "fragment.glsl");
+    auto mod = glm::translate(glm::mat4(1.0), glm::vec3(-0.25));
+    auto view = glm::mat4(1.0f);
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -2.0f));
+    glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)1920/(float)1080, 0.1f, 100.0f);
 
     // model
-    Model model(vertices, sizeof(vertices), 3);
+    Model model(vertices, indices, sizeof(vertices), sizeof(indices), 36);
     model.addAttribute(0, pos);
     model.addAttribute(1, col);
     model.setProgram(&program);
+
+    sf::Vector2i prev = sf::Mouse::getPosition();
+    float angleX = 0, angleY = 0;
 
     while (window.isOpen())
     {
@@ -158,10 +242,30 @@ int main()
             {
                 window.close();
             }
+            else if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                prev = sf::Vector2i(event.mouseButton.x, event.mouseButton.y);
+                std::cout << prev.x << '\n';
+                std::cout << prev.y << '\n';
+            }
+            else if (event.type == sf::Event::MouseMoved && sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                std::cout << event.mouseMove.x << '\n';
+                std::cout << event.mouseMove.y << '\n';
+                prev = sf::Vector2i(event.mouseMove.x, event.mouseMove.y) - prev;
+                angleX += prev.x / 1920.0f * 90.0f;
+                angleY += prev.y / 1080.0f * 90.0f;
+                mod = glm::mat4(1.0);
+                mod = glm::rotate(mod, glm::radians(angleX), glm::vec3(0.0, 1.0, 0.0));
+                mod = glm::rotate(mod, glm::radians(angleY), glm::vec3(1.0, 0.0, 0.0));
+                mod = glm::translate(mod, glm::vec3(-0.25));
+                prev = sf::Vector2i(event.mouseMove.x, event.mouseMove.y);
+            }
         }
 
         glClearColor(0, 0.5, 0.5, 1);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        program.setUniformMatrix4fv("model", mod);
+        program.setUniformMatrix4fv("view", view);
+        program.setUniformMatrix4fv("projection", proj);
         model.draw();
         window.display();
     }
--- cube-lib/src/vertex.glsl
+++ cube-lib/src/vertex.glsl
@@ -4,8 +4,12 @@ layout (location = 1) in vec3 color;
 
 out vec3 colorOut;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
     colorOut = color;
 }
