1d5b817ab2d302ee36f7a215f8a522670b6bc28d
Snoake1 <pavelbogatirov@mail.ru>
2024-03-04 14:53:07 +0300

    add buttons in mainactivity

---
+++ app/src/main/java/com/example/intcube/SelectColorsActivity.java
@@ -0,0 +1,14 @@
+package com.example.intcube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectColorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_colors);
+    }
+}
