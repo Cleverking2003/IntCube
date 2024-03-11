f3af98fc100b4694bd9d71a909ac414e633c396b
Snoake1 <pavelbogatirov@mail.ru>
2024-03-11 17:02:18 +0300

    added new activities

--- app/src/main/java/com/example/intcube/MainActivity.java
+++ app/src/main/java/com/example/intcube/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.intcube;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonToScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void startActivityScan(View v){
+        Intent intent = new Intent(this, ScanTypeActivity.class);
+        startActivity(intent);
+    }
 
+    public void startActivityGuide(View v){
+        Intent intent = new Intent(this, GuideActivity.class);
+        startActivity(intent);
+    }
+
+    public void startActivityHelp(View v){
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
+    }
 }
