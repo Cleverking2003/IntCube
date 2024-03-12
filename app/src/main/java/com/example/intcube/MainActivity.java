d7f2b8020c1629d052590bf8fa92651ed8de822e
Snoake1 <pavelbogatirov@mail.ru>
2024-03-12 11:14:23 +0300

    failed

--- app/src/main/java/com/example/intcube/MainActivity.java
+++ app/src/main/java/com/example/intcube/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.intcube;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (OpenCVLoader.initLocal()) {
+            Log.i(TAG, "OpenCV loaded successfully");
+        } else {
+            Log.e(TAG, "OpenCV initialization failed!");
+            return;
+        }
     }
 
     public void startActivityScan(View v){
