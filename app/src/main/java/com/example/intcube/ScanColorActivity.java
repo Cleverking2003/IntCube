7c63f780fe37296343a0f8351ff42023fd58862f
Snoake1 <pavelbogatirov@mail.ru>
2024-03-18 16:08:55 +0300

    add permission in scanType

--- app/src/main/java/com/example/intcube/ScanColorActivity.java
+++ app/src/main/java/com/example/intcube/ScanColorActivity.java
@@ -1,17 +1,74 @@
 package com.example.intcube;
 
+import static org.opencv.android.NativeCameraView.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
-public class ScanColorActivity extends AppCompatActivity {
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
+public class ScanColorActivity extends AppCompatActivity{
+
+    private static final int CAMERA_PERMISSION_CODE = 100;
+
+    private CameraBridgeViewBase cameraBridgeViewBase;
+
+    private Mat mat1, mat2, mat3;
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_color);
+
+        //! [ocv_loader_init]
+        if (OpenCVLoader.initLocal()) {
+            Log.i(TAG, "OpenCV loaded successfully");
+        } else {
+            Log.e(TAG, "OpenCV initialization failed!");
+            (Toast.makeText(this, "OpenCV initialization failed!", Toast.LENGTH_LONG)).show();
+        }
+
+        checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE);
+    }
+
+    public void checkPermission(String permission, int requestCode)
+    {
+        if (ContextCompat.checkSelfPermission(ScanColorActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+            // Requesting the permission
+            ActivityCompat.requestPermissions(ScanColorActivity.this, new String[] { permission }, requestCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode,
+                permissions,
+                grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(ScanColorActivity.this, "Camera Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(ScanColorActivity.this, "Для сканирования необходимо разрешение для использования камеры", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     public void BackActivityScanType(View v){
@@ -20,7 +77,7 @@ public class ScanColorActivity extends AppCompatActivity {
     }
 
     public void StartActivitySolution(View v){
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, SolutionActivity.class);
         startActivity(intent);
     }
 }
