ebf2560e4cdcbfe4f8e70fad433935a991cbc67e
Даниил <dan_ptz@mail.ru>
2024-03-21 14:32:09 +0300

    Try to add Guides pages

--- app/src/main/java/com/example/intcube/GuideActivity.java
+++ app/src/main/java/com/example/intcube/GuideActivity.java
@@ -2,7 +2,9 @@ package com.example.intcube;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GuideActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class GuideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+    }
+
+    public void startActivity2x2G(View v){
+        Intent intent = new Intent(this, Guide2x2Activity.class);
+        startActivity(intent);
+    }
+
+    public void startActivity3x3G(View v){
+        Intent intent = new Intent(this, Guide3x3Activity.class);
+        startActivity(intent);
+    }
+
+    public void startActivity4x4G(View v){
+        Intent intent = new Intent(this, Guide4x4Activity.class);
+        startActivity(intent);
+    }
+
+    public void startActivityAxisG(View v){
+        Intent intent = new Intent(this, GuideAxisActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/intcube/GuideAxisActivity.java
@@ -0,0 +1,14 @@
+package com.example.intcube;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GuideAxisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide_axis);
+    }
+}
