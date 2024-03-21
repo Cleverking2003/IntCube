5cd971a3f3207dda1ae53e24d366d0abc64c15d4
Даниил <dan_ptz@mail.ru>
2024-03-21 14:32:09 +0300

    Try to add Guides pages

---
+++ app/src/main/java/com/example/intcube/Guide2x2Activity.java
@@ -0,0 +1,15 @@
+package com.example.intcube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Guide2x2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide_2x2);
+    }
+}
