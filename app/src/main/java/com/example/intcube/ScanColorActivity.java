2f6e550e3a7b13b8c35d65543709ef861ffe5134
Snoake1 <pavelbogatirov@mail.ru>
2024-03-28 22:01:23 +0300

    fix cameraview

--- app/src/main/java/com/example/intcube/ScanColorActivity.java
+++ app/src/main/java/com/example/intcube/ScanColorActivity.java
@@ -1,75 +1,114 @@
 package com.example.intcube;
 
-import static org.opencv.android.NativeCameraView.TAG;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
+import org.opencv.android.CameraActivity;
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
-public class ScanColorActivity extends AppCompatActivity{
+import java.util.Collections;
+import java.util.List;
 
-    private static final int CAMERA_PERMISSION_CODE = 100;
+public class ScanColorActivity extends CameraActivity implements CvCameraViewListener2 {
+    private static final String TAG = "OCVSample::Activity";
 
-    private CameraBridgeViewBase cameraBridgeViewBase;
+    private CameraBridgeViewBase mOpenCvCameraView;
 
-    private Mat mat1, mat2, mat3;
+    public ScanColorActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan_color);
 
-        //! [ocv_loader_init]
         if (OpenCVLoader.initLocal()) {
             Log.i(TAG, "OpenCV loaded successfully");
         } else {
             Log.e(TAG, "OpenCV initialization failed!");
             (Toast.makeText(this, "OpenCV initialization failed!", Toast.LENGTH_LONG)).show();
+            return;
         }
 
-        checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE);
-    }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-    public void checkPermission(String permission, int requestCode)
-    {
-        if (ContextCompat.checkSelfPermission(ScanColorActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
-            // Requesting the permission
-            ActivityCompat.requestPermissions(ScanColorActivity.this, new String[] { permission }, requestCode);
-        }
+        setContentView(R.layout.activity_scan_color);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_java_surface_view);
+
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+
+        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults)
+    public void onPause()
     {
-        super.onRequestPermissionsResult(requestCode,
-                permissions,
-                grantResults);
-
-        if (requestCode == CAMERA_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(ScanColorActivity.this, "Разрешение на использование камеры получено", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(ScanColorActivity.this, "Для сканирования необходимо разрешение для использования камеры", Toast.LENGTH_SHORT).show();
-            }
-        }
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
     }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.enableView();
+    }
+
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(mOpenCvCameraView);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+    }
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        Mat mRgba = inputFrame.rgba();
+        int w = mRgba.width();
+        int h = mRgba.height();
+        Imgproc.rectangle(mRgba, new Point((double) w / 4,  (double) h / 4), new Point(
+                 (double) (w * 2) / 3,  (double) (h * 2) / 3), Scalar.all(255.0));
+        Rect roi = new Rect(new Point((double) w / 3,  (double) h / 3), new Point(
+                (double) (w * 2) / 3,  (double) (h * 2) / 3));
+        Mat viewFinder = mRgba.submat(roi);
+        return mRgba;
+    }
+
 
     public void BackActivityScanType(View v){
         Intent intent = new Intent(this, ScanTypeActivity.class);
