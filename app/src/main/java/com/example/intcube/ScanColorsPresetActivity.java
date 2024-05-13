199c9d0eafbf7fb05b7833ba7fc185abb5dff3e4
Snoake1 <pavelbogatirov@mail.ru>
2024-05-13 21:31:50 +0300

    add some res

---
+++ app/src/main/java/com/example/intcube/ScanColorsPresetActivity.java
@@ -0,0 +1,111 @@
+package com.example.intcube;
+
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+
+import android.widget.Toast;
+
+
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.OpenCVLoader;
+
+import org.opencv.core.Mat;
+
+
+import java.util.Collections;
+import java.util.List;
+
+
+public class ScanColorsPresetActivity extends CameraActivity implements CvCameraViewListener2 {
+    private static final String TAG = "OCVSample::Activity";
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private int counter = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+
+        if (OpenCVLoader.initLocal()) {
+            Log.i(TAG, "OpenCV loaded successfully");
+        } else {
+            Log.e(TAG, "OpenCV initialization failed!");
+            (Toast.makeText(this, "OpenCV initialization failed!", Toast.LENGTH_LONG)).show();
+            return;
+        }
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_scan_colors_preset);
+
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.CameraView2);
+
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.enableView();
+    }
+
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(mOpenCvCameraView);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+    }
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        Mat mRgba = inputFrame.rgba();
+        return mRgba;
+    }
+
+    public void BackActivityScanType(View v){
+        Intent intent = new Intent(this, ScanTypeActivity.class);
+        startActivity(intent);
+    }
+
+    public void ScanColor(View v){
+        counter++;
+    }
+}
