43588237f27dc660060e07948a6d57a152886c2d
Даниил <dan_ptz@mail.ru>
2024-05-20 13:39:21 +0300

    Add Note to guide, remove En in lang spinner

--- app/src/main/java/com/example/intcube/GuideActivity.java
+++ app/src/main/java/com/example/intcube/GuideActivity.java
@@ -1,16 +1,22 @@
 package com.example.intcube;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.preference.PreferenceManager;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class GuideActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,17 +25,38 @@ public class GuideActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.guideToolbar);
 
         setSupportActionBar(toolbar);
-        toolbar.setTitleTextColor(android.graphics.Color.WHITE);
+        //toolbar.setTitleTextColor(android.graphics.Color.WHITE);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
             getSupportActionBar().setTitle("Обучающие курсы");
         }
     }
 
-    public boolean onSupportNavigateUp() {
-        this.finish();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.window_info_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.action_info:
+                AlertDialog dialogInfo = (AlertDialog) createDialog();
+                dialogInfo.show();
+                return true;
+        }
+        return true;
+    }
+
+    public Dialog createDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Справка")
+                .setMessage(R.string.guideNote);
+        return builder.create();
     }
 
     public void startActivity2x2G(View v){
--- app/src/main/java/com/example/intcube/SettingsActivity.java
+++ app/src/main/java/com/example/intcube/SettingsActivity.java
@@ -1,22 +1,32 @@
 package com.example.intcube;
 
+import android.app.Dialog;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        String[] languages = {"Ru", "En"};
+        // Spinner выбора языка
+        String[] languages = {"Ru"};
         Spinner spinner = findViewById(R.id.langSpinner);
         ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, languages);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -25,18 +35,7 @@ public class SettingsActivity extends AppCompatActivity {
         SwitchCompat themeSwitch = findViewById(R.id.themeSwitch);
         Toolbar toolbar = findViewById(R.id.guideToolbar);
 
-        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES)
-            themeSwitch.setChecked(true);
-
-        setSupportActionBar(toolbar);
-        toolbar.setTitleTextColor(android.graphics.Color.WHITE);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-            getSupportActionBar().setTitle("Настройки");
-        }
-
+        // Обработка переключения Switch
         if (themeSwitch != null) {
             themeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
                 if (isChecked)
@@ -45,6 +44,20 @@ public class SettingsActivity extends AppCompatActivity {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
             });
         }
+
+        // Установка switch'a в нужное положение
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+            themeSwitch.setChecked(true);
+        }
+
+        // Подключение Toolbar'a
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle("Настройки");
+        }
     }
 
     public boolean onSupportNavigateUp() {
@@ -52,3 +65,4 @@ public class SettingsActivity extends AppCompatActivity {
         return true;
     }
 }
+
